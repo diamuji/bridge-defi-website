40655efa7306d7d8bf85cd5d36036e67946bfbc7
diamuji <smt.dev0502@gmail.com>
2021-04-11 11:33:16 +0200

    Revert "Fix import"
    This reverts commit 1ca1c38547af021be8a4e01008f96d5b28dc4757.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Home from './pages/Home';
 import Help from './pages/Help';
 import PageNotFound from './pages/PageNotFound';
 import SignUp from './pages/SignUp/SignUp';
-import SignIn from './pages/SignIn';
+import SignIn from './pages/Signin';
 import SignOut from './pages/SignOut';
 import Confirmation from './pages/Confirmation';
 import App from './pages/App';
