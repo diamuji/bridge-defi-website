f72505b45970344f08318d20ff345770e0c585bf
diamuji <smt.dev0502@gmail.com>
2021-04-26 01:59:35 +0200

    Portfolio: fix error for missing portfolios

--- src/pages/Dashboard/Portfolio.js
+++ src/pages/Dashboard/Portfolio.js
@@ -5,6 +5,7 @@ import { http } from '../../utils/utils';
 
 export default function Portfolio(props) {
     const [portfolio, setPortfolio] = useState();
+    const myPortfolio = portfolio.portfolio || {};
 
     useEffect(() => {
         const fetchData = async () => {
@@ -28,7 +29,7 @@ export default function Portfolio(props) {
                     <div key={currency.symbol} className="flex flex-row items-center py-2 border-b border-gray-200 last:border-b-0">
                         {currency.image()}
                         <div className="ml-3 leading-4">
-                            {portfolio.portfolio[currency.symbol.toLowerCase()]}
+                            {myPortfolio[currency.symbol.toLowerCase()] || 0}
                             <div className="text-gray-500 text-xs">{currency.symbol}</div>
                         </div>
                     </div>
