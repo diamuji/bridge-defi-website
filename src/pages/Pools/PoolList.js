400068b4f4b0340bccca651a9a9e79d27acb38d0
diamuji <smt.dev0502@gmail.com>
2021-04-30 02:02:45 +0200

    List of pools

---
+++ src/pages/Pools/PoolList.js
@@ -0,0 +1,63 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import Loading from '../../partials/Loading';
+import Table from '../../partials/Table/Table';
+import TableBody from '../../partials/Table/TableBody';
+import TableCell from '../../partials/Table/TableCell';
+import TableHead from '../../partials/Table/TableHead';
+import TableRow from '../../partials/Table/TableRow';
+import { http } from '../../utils/utils';
+
+export default function PoolList() {
+    const [pools, setPools] = useState();
+
+    const fetchPools = useCallback(async () => {
+        const pools = await http({
+            method: 'GET',
+            url: '/pool',
+        });
+        setPools(pools || []);
+    }, []);
+
+    useEffect(() => {
+        fetchPools();
+    }, [fetchPools]);
+
+    return (
+        <Loading if={!pools}>
+            <Table title="Pools">
+                <TableHead>
+                    <TableRow>
+                        <TableCell header>Name</TableCell>
+                        <TableCell header>APY Monthly</TableCell>
+                        <TableCell header>APY Yearly</TableCell>
+                        <TableCell header>Available</TableCell>
+                        <TableCell header>Sold</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {!pools?.length && (
+                        <TableRow>
+                            <TableCell colSpan="4">
+                                <i>No results</i>
+                            </TableCell>
+                        </TableRow>
+                    )}
+                    {(pools || []).map(pool => (
+                        <TableRow key={pool._id}>
+                            <TableCell>
+                                {pool.name}
+                                <div className={`text-xs ${pool.active ? 'text-green-500' : 'text-red-500'}`}>
+                                    {pool.active ? 'active' : 'not active'}
+                                </div>
+                            </TableCell>
+                            <TableCell>{pool.apyMonthly}</TableCell>
+                            <TableCell>{pool.apyYearly}</TableCell>
+                            <TableCell>{pool.balanceAvailable}</TableCell>
+                            <TableCell>{pool.balanceSold}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Loading>
+    );
+}
--- src/pages/Pools/Pools.js
+++ src/pages/Pools/Pools.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { useHistory, withRouter } from 'react-router-dom';
 import LoggedPage from '../../partials/LoggedPage';
+import PoolList from './PoolList';
 
 function Pools() {
     const history = useHistory();
 
     return (
         <LoggedPage>
-            <div className="sm:flex sm:justify-between sm:items-center mb-8">
-                <div className="mb-4 sm:mb-0">
-                    <h1 className="text-2xl text-gray-800 font-bold">
-                        Pools
-                    </h1>
-                </div>
+            <div className="mb-4 sm:mb-0 flex flex-row mb-8">
+                <h1 className="text-2xl text-gray-800 font-bold flex-grow">
+                    Pools
+                </h1>
+                <button
+                    type="submit"
+                    className="btn text-white bg-teal-500 hover:bg-teal-600 px-10 mb-5"
+                    onClick={() => history.push('/pools/create')}
+                >
+                    Create new
+                </button>
             </div>
 
-            <button type="submit" className="btn text-white bg-teal-500 hover:bg-teal-600 px-20" onClick={() => history.push('/pools/create')}>
-                Create new
-            </button>
+            <PoolList />
 
         </LoggedPage>
     );
